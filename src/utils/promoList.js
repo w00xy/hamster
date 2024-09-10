44e4764b6e54537ae9e8eebf1953c1f06d8282e6
w00xy <vlad42006@mail.com>
2024-09-10 22:00:56 +0300

    Added new recent games

--- src/utils/promoList.js
+++ src/utils/promoList.js
@@ -1,9 +1,55 @@
 export const promoList = {
+  'stone-age' : {
+    APP_TOKEN: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+    PROMO_ID: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+    DELAY: '22000'
+  },
+  'train-game' : {
+    APP_TOKEN: '82647f43-3f87-402d-88dd-09a90025313f',
+    PROMO_ID: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
+    DELAY: '30000'
+  },
+  'mow-and-trim' : {
+    APP_TOKEN: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+    PROMO_ID: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+    DELAY: '20000',
+  },
+  'chain-game' : {
+    APP_TOKEN: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
+    PROMO_ID: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
+    DELAY: '28000'
+  },
+  'fluff-crusade' : {
+    APP_TOKEN: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+    PROMO_ID: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+    DELAY: '20000'
+  },
+  'merge-away' : {
+    APP_TOKEN: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
+    PROMO_ID: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
+    DELAY: '30000'
+  },
   'polysphere' : {
     APP_TOKEN: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
     PROMO_ID: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+    DELAY: '20000'
+  },
+  'twerk-race' : {
+    APP_TOKEN: '61308365-9d16-4040-8bb0-2f4a4c69074c',
+    PROMO_ID: '61308365-9d16-4040-8bb0-2f4a4c69074c',
     DELAY: '30000'
   },
+  'zoopolis' : {
+    APP_TOKEN: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+    PROMO_ID: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+    DELAY: '20000',
+  },
+  'tile-trio' : {
+    APP_TOKEN: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+    PROMO_ID: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+    DELAY: '20000',
+  }
+
   // 'bike-game' : {
   //   APP_TOKEN: 'd28721be-fd2d-4b45-869e-9f253b554e50',
   //   PROMO_ID: '43e35910-c168-4634-ad4f-52fd764a843f',
@@ -14,45 +60,10 @@ export const promoList = {
   //   PROMO_ID: 'fe693b26-b342-4159-8808-15e3ff7f8767',
   //   DELAY: '30000'
   // },
-  'twerk-race' : {
-    APP_TOKEN: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-    PROMO_ID: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-    DELAY: '30000'
-  },
-  'merge-away' : {
-    APP_TOKEN: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
-    PROMO_ID: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
-    DELAY: '30000'
-  },
-  'train-game' : {
-    APP_TOKEN: '82647f43-3f87-402d-88dd-09a90025313f',
-    PROMO_ID: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-    DELAY: '30000'
-  },
-  'chain-game' : {
-    APP_TOKEN: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-    PROMO_ID: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-    DELAY: '30000'
-  },
-  'mow-and-trim' : {
-    APP_TOKEN: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-    PROMO_ID: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
-    DELAY: '20000',
-  },
-
   // 'mud-racing' : {
   //   APP_TOKEN: '8814a785-97fb-4177-9193-ca4180ff9da8',
   //   PROMO_ID: '8814a785-97fb-4177-9193-ca4180ff9da8',
   //   DELAY: '20000',
   // },
-  'cafe-dash': {
-    APP_TOKEN: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-    PROMO_ID: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-    DELAY: '20000',
-  },
-  'zoopolis' : {
-    APP_TOKEN: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-    PROMO_ID: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
-    DELAY: '20000',
-  }
+
 }
